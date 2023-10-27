6cf8170ed9123fd2dc555b83c9a1b5e83a72b195
buidangkhoa97 <140301236+buidangkhoa97@users.noreply.github.com>
2023-10-27 20:14:09 +0900

    Add files via upload

---
+++ Assignment 03 Stater/Assignment 03 Stater-1/javascript/script.js
@@ -0,0 +1,5 @@
+function handleSubmit() {
+  const emailValue = document.getElementById("email");
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+}
